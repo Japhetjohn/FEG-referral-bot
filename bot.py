e2b76c69dea904990676647616d07ced30cdbbf7
japhet john <japhetjohnk@gmail.com>
2025-02-25 00:55:15 +0100

    updated bot structure

--- bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-BOT_TOKEN = "7621286461:AAFG7MCDH4o4q7_xtWl1wI-e0Ts0lEZvmiA"
-bot = telebot.TeleBot(BOT_TOKEN)
-
-def start_bot():
-    bot.polling(none_stop=True)
-
-start_bot()
--- utils.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(filename="logs/bot.log", level=logging.INFO)
-
-def log_event(event):
-    """Logs bot events to a file."""
-    logging.info(event)
